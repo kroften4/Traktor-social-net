a9e064312aec1f718322b51bcbd3908ccd4b2221
kroften4 <134956188+kroften4@users.noreply.github.com>
2024-10-02 22:46:27 +0300

    Small tweaks to search
    - Move `search-users` url path from posts to main app
    - Add styles to `search.html`
    - Symplify `get_queryset` func in search/views.py
    - Attempt to fix search query not returning all results (messing with `%` around query)

--- traktor_sn/posts/urls.py
+++ traktor_sn/posts/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     re_path(r'^get_image/$', get_image),
     path('', PostListView.as_view(), name='posts-home'),
     path('post/new', PostCreateView.as_view(), name='post-create'),
-    path('search/', SearchView.as_view(), name='search-users')
 ]
--- traktor_sn/search/views.py
+++ traktor_sn/search/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.models import User
 
+
 # Create your views here.
 
 class SearchView(ListView):
@@ -10,12 +11,15 @@ class SearchView(ListView):
     context_object_name = 'all_search_results'
 
     def get_queryset(self):
-       result = super(SearchView, self).get_queryset()
-       query = self.request.GET.get('search')
-       if query:
-           #SQL INJECTION
-          postresult = User.objects.raw(f"SELECT * FROM auth_user WHERE username LIKE '%{query}%'")
-          result = postresult
-       else:
-           result = None
-       return result
+        query = self.request.GET.get('search')
+        result = None
+        if query:
+            # SQL INJECTION
+            result = User.objects.raw(
+                f"SELECT * "
+                f"FROM auth_user "
+                f"WHERE username LIKE '%{query}' "
+                f"OR username LIKE '{query}%'"
+                f"OR username LIKE '%{query}%'"
+            )
+        return result

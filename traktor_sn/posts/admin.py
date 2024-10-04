e439a5410a3a08dd7d4a5f4a7cba551322206465
kroften4 <134956188+kroften4@users.noreply.github.com>
2024-10-04 09:06:28 +0300

    Add flag{} placeholder to admin panel

--- traktor_sn/posts/admin.py
+++ traktor_sn/posts/admin.py
@@ -4,3 +4,6 @@ from .models import Post
 # Register your models here.
 
 admin.site.register(Post)
+
+# TODO: generate an actual flag later
+admin.site.site_header = 'Congrats! flag{asdfyao8wefga8734yr2ykr783rgk84384tg}'
